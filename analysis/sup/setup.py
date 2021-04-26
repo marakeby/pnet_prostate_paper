0ce8706edd40b2a5eab27ccf2d051cbc96398ab5
marakeby <marakeby@vt.edu>
2021-04-26 05:29:30 -0400

    fix typo

--- analysis/sup/setup.py
+++ analysis/sup/setup.py
@@ -5,6 +5,6 @@ current_dir = dirname(dirname(realpath(__file__)))
 sys.path.insert(0, dirname(current_dir))
 from config_path import PLOTS_PATH
 
-saving_dir = join(PLOTS_PATH, 'suplmentary')
+saving_dir = join(PLOTS_PATH, 'supplementary')
 if not exists(saving_dir):
     makedirs(saving_dir)
